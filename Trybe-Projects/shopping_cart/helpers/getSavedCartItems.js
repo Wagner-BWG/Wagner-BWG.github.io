bf4ba0a3529cc092e4ad88cebc846034c3d8624e
Wagner M Barbosa <wagner_mbarbosa@hotmail.com>
2024-05-23 19:14:57 -0300

    Completely revamped this thing

--- Trybe-Projects/shopping_cart/helpers/getSavedCartItems.js
+++
@@ -1,9 +0,0 @@
-const getSavedCartItems = () => {
-  const savedData = localStorage.getItem('cartItems');
-  const savedPrices = localStorage.getItem('itemsValues');
-  return [savedData, savedPrices];
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = getSavedCartItems;
-}
--- Trybe-Projects/shopping_cart/helpers/saveCartItems.js
+++
@@ -1,7 +0,0 @@
-const saveCartItems = (cart) => {
-  localStorage.setItem('cartItems', JSON.stringify(cart));
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
